903f1f88ad67c0a414d4d1f2075db41f1984d0c8
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-09-21 09:57:12 +0200

    sitemap with right url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `new`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.zenith-skishop.com`,
   },
   plugins: [
     "gatsby-plugin-image",
